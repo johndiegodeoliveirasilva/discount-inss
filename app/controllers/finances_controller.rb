335116a56fed28c26c61942e3380d3b72c997bdc
johndiegodeoliveirasilva <johnoliveirasilva9@gmail.com>
2024-11-01 19:14:35 -0300

    add function to calculate tax

--- app/controllers/finances_controller.rb
+++ app/controllers/finances_controller.rb
@@ -2,5 +2,14 @@ class FinancesController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @proposer_id = params[:proposer_id]
+  end
+
+  def calculate
+    render json: { amount: rand(151) }
+  end
+
+  def sync_tax
+    render json: { message: "Taxa sincronizada com sucesso." }, status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :proposers, only: %w[create new index show] do
-    resources :finances, only: :new
+    get "finances/calculate", to: "finances#calculate"
+    post "finances/sync_tax", to: "finances#sync_tax"
+
+    resources :finances, only: %w[new] do
+    end
   end
 
   get "report", to: "reports#show", as: "report_screen"
