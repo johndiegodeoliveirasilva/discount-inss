eb23de04e2dc619f54d86773ccce3bb143c09b9d
johndiegodeoliveirasilva <johnoliveirasilva9@gmail.com>
2024-11-01 13:01:57 -0300

    restyle navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,14 @@
 
 module ApplicationHelper
   def login_helper(style = '')
-    (link_to "Register", new_user_registration_path, class: style) +
-    " ".html_safe +
-    (link_to "Login", new_user_session_path, class: style) unless user_signed_in?
-    link_to "Logout", destroy_user_session_path, method: :delete, class: style if user_signed_in?
+    if user_signed_in?
+      (link_to "Edit Perfil", edit_user_registration_path, class: style) + " ".html_safe +
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
+    else
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
+    end
   end
 
   def nav_items
