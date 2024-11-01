ae8c9f7451a2b57e37cbb2afb5170b5f0314cb98
johndiegodeoliveirasilva <johnoliveirasilva9@gmail.com>
2024-11-01 09:00:35 -0300

    create migration proposer and fix table columns

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name document birth_date])
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_01_014115) do
+ActiveRecord::Schema.define(version: 2024_11_01_114956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+  create_table "proposers", force: :cascade do |t|
+    t.bigint "user_id"
     t.string "full_name"
     t.string "document"
     t.datetime "birth_date"
     t.float "income"
+    t.index ["user_id"], name: "index_proposers_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -31,4 +36,5 @@ ActiveRecord::Schema.define(version: 2024_11_01_014115) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "proposers", "users"
 end
