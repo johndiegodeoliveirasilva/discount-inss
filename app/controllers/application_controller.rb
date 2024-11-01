7425aa36270ef7e38e6dcbc86b1b19a66e4a24ac
johndiegodeoliveirasilva <johnoliveirasilva9@gmail.com>
2024-10-31 23:18:03 -0300

    update form sign up devise adding new fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name document birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name document birth_date])
   end
 end
