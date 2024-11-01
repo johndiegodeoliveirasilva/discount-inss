d71ea6e60c539c19747f01e520d963889b09ad79
johndiegodeoliveirasilva <johnoliveirasilva9@gmail.com>
2024-11-01 09:15:58 -0300

    fix table with proposers

--- db/migrate/20241101125513_create_addresses.rb
+++ db/migrate/20241101125513_create_addresses.rb
@@ -1,7 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.references :user, foreign_key: true
+      t.references :proposer, foreign_key: true
       t.string :complement
       t.string :number
       t.string :neighborhood
