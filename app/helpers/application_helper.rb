96ce292cc4eb9775e4394c3182316090aab20971
John Diego de Oliveira Silva <johnoliveirasilva9@gmail.com>
2024-11-01 12:52:08 -0300

    Refactor login_helper method and add transparent background style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def login_helper(style = '')
+    (link_to "Register", new_user_registration_path, class: style) +
+    " ".html_safe +
+    (link_to "Login", new_user_session_path, class: style) unless user_signed_in?
+    link_to "Logout", destroy_user_session_path, method: :delete, class: style if user_signed_in?
+  end
+
   def nav_items
     [
       {
