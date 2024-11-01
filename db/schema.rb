e5f43388fbda4f913f7af25dd30e21e461c23f17
johndiegodeoliveirasilva <johnoliveirasilva9@gmail.com>
2024-11-01 00:09:51 -0300

    create user and address on seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_241_101_014_115) do
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'full_name'
-    t.string 'document'
-    t.datetime 'birth_date'
-    t.float 'income'
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+ActiveRecord::Schema.define(version: 2024_11_01_025513) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "complement"
+    t.string "number"
+    t.string "neighborhood"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "full_name"
+    t.string "document"
+    t.datetime "birth_date"
+    t.float "income"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating User Default" 
+
+user = User.create(
+  email: "default@gmail.com",
+  full_name: "Default",
+  birth_date: "01/01/1990",
+  password: "123456",
+  password_confirmation: "123456",
+  document: '69423555420'
+)
+
+Address.create!(
+  user: user,
+  complement: '123 Rua Principal',
+  number: '1',
+  city: 'Santiago',
+  state: 'RM',
+  zip_code: '12345',
+  neighborhood: '1'
+)
