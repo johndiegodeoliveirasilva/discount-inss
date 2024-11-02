5bfd5fce9eac7e2617ffa9e7ba0ecf30dbada9cf
John Diego de Oliveira Silva <johnoliveirasilva9@gmail.com>
2024-11-02 15:04:28 -0300

    applied sujestions by rubocop on test calculate_spec

--- spec/services/finances/inss/calculate_spec.rb
+++ spec/services/finances/inss/calculate_spec.rb
@@ -1,59 +1,61 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe Finances::Inss::Calculate do
-  describe "#call" do
+  describe '#call' do
     let(:subject) { described_class.new(salary) }
 
-    context "when base salary is 0" do
+    context 'when base salary is 0' do
       let(:salary) { 0 }
-      it "returns 0" do
+      it 'returns 0' do
         expect(subject.call).to eq(0)
       end
     end
 
-    context "when base salary is 1045.00" do
+    context 'when base salary is 1045.00' do
       let(:salary) { 1045.00 }
-      it "returns 78.38" do
+      it 'returns 78.38' do
         expect(subject.call).to eq(78.37)
       end
     end
 
-    context "when base salary is 1045.01" do
+    context 'when base salary is 1045.01' do
       let(:salary) { 1045.01 }
 
-      it "returns 78.37" do
+      it 'returns 78.37' do
         expect(subject.call).to eq(78.37)
       end
     end
 
-    context "when base salary is 2089.60" do
+    context 'when base salary is 2089.60' do
       let(:salary) { 2089.60 }
 
-      it "returns 172.38" do
+      it 'returns 172.38' do
         expect(subject.call).to eq(172.38)
       end
     end
 
-    context "when base salary is 3000" do
+    context 'when base salary is 3000' do
       let(:salary) { 3000 }
 
-      it "returns 281.62" do
+      it 'returns 281.62' do
         expect(subject.call).to eq(281.62)
       end
     end
 
-    context "when base salary is 2500" do
+    context 'when base salary is 2500' do
       let(:salary) { 2500 }
 
-      it "returns 221.62" do
+      it 'returns 221.62' do
         expect(subject.call).to eq(221.62)
       end
     end
 
-    context "when base salary is 6101.06" do
+    context 'when base salary is 6101.06' do
       let(:salary) { 6101.06 }
 
-      it "returns 713.08" do
+      it 'returns 713.08' do
         expect(subject.call).to eq(713.08)
       end
     end
