64ddc3497e5040e06e039e2b672e867cf95274d7
John Diego de Oliveira Silva <johnoliveirasilva9@gmail.com>
2024-11-02 20:06:16 -0300

    update factory bot

--- spec/factories/proposer.rb
+++ spec/factories/proposer.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     document { Faker::IDNumber.brazilian_citizen_number }
     birth_date { Faker::Date.birthday(min_age: 18, max_age: 65) }
     income { Faker::Number.decimal(l_digits: 4, r_digits: 2) }
+    user
   end
 end
